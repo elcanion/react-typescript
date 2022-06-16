5368da797da7242863c910262f16e4102d6f3c8e
elcanion <beethovenssixth@gmail.com>
2022-06-16 14:20:41 -0300

    add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 import { Navbar } from './components/Navbar';
 import { Title } from './components/Title';
 
 
 function App() {
+  const [count, setCount] = useState(0);
+
+
+
   return (
     <div>
       <Navbar/>
       <Title color='red' variant='md'>React.js live</Title>
+      <p>Click Counter: {count}</p>
+      <button onClick={(e) => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
