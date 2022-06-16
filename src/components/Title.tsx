5f53fd6fa26c9490c7336f573a5265f555cf7150
elcanion <beethovenssixth@gmail.com>
2022-06-16 13:42:41 -0300

    first commit

---
+++ src/components/Title.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from "react"
+
+type TitleProps = {
+    color?: string;
+}
+
+export function Title(props: PropsWithChildren<TitleProps>) {
+    const { color = 'green', children } = props;
+    return <h1 style={{ color }}>{children}</h1>
+}
