4399ec1fa1386095038c9ffa69ecc9b2539f8c31
elcanion <beethovenssixth@gmail.com>
2022-06-16 15:17:20 -0300

    changing component styling using state

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,41 @@
 import { useState } from "react";
 
 export function Counter() {
-    const [count, setCount] = useState(0);
+    const [fgCount, setFgCount] = useState(255);
+
+    const [redCount, setRedCount] = useState(0);
+    const [greenCount, setGreenCount] = useState(0);
+    const [blueCount, setBlueCount] = useState(0);
     
     return (
         <div>
             <p>
-                Click Counter: 
-                <span style={{backgroundColor: `rgb(${count}, ${count}, ${count})`}}>{count}</span>
+                Color swap: <span>r{redCount},g{greenCount},b{blueCount},fg{fgCount}</span>
+                <span style={{
+                    backgroundColor: `rgb(${redCount}, ${greenCount}, ${blueCount})`,
+                    color: `rgb(${fgCount}, ${fgCount}, ${fgCount})`,
+                    }}>***</span>
             </p>
-            <button onClick={(e) => setCount(count + 1)}>Click me</button>
+            <div>
+                <button onClick={(e) => setRedCount(redCount + 1)}>R + 1</button>
+                <button onClick={(e) => setGreenCount(greenCount + 1)}>G + 1</button>
+                <button onClick={(e) => setBlueCount(blueCount + 1)}>B + 1</button>
+            </div>
+            <div>
+                <button onClick={(e) => setRedCount(redCount - 1)}>R - 1</button>
+                <button onClick={(e) => setGreenCount(greenCount - 1)}>G - 1</button>
+                <button onClick={(e) => setBlueCount(blueCount - 1)}>B - 1</button>
+            </div>
+            <div>
+                <button onClick={(e) => setFgCount(fgCount - 1)}>Fg - 1</button>
+                <button onClick={(e) => setFgCount(fgCount - 10)}>Fg - 10</button>
+                <button onClick={(e) => setFgCount(fgCount - 20)}>Fg - 20</button>
+            </div>
+            <div>
+                <button onClick={(e) => setFgCount(fgCount + 1)}>Fg + 1</button>
+                <button onClick={(e) => setFgCount(fgCount + 10)}>Fg + 10</button>
+                <button onClick={(e) => setFgCount(fgCount + 20)}>Fg + 20</button>
+            </div>
         </div>
     )
 }
